2f8ba301f60fb31e334e050038eb860f1cabb689
xuya227939 <1245244711@qq.com>
2018-05-25 16:16:46 +0800

    新增一个密码

--- conf/mysql.js
+++ conf/mysql.js
@@ -6,7 +6,7 @@ const myConnection = require('express-myconnection');
 const mysqlConf = myConnection(mysql, {
   host: 'localhost',
   user: 'root',
-  password: '',
+  password: 'xuya227939',
   port: 3306,
   database: 'shareimage'
 }, 'single');
